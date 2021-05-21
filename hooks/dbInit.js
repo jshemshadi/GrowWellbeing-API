aa04d7c8ab65901165505f06b4a018a53b286877
jshemshadi <jshemshadi@gmail.com>
2021-05-21 10:40:02 +0430

    Add main admin

--- hooks/dbInit.js
+++ hooks/dbInit.js
@@ -1,1 +1,37 @@
-module.exports = async () => {};
+const userModel = require("../models/User");
+
+module.exports = async () => {
+  const { users } = db;
+
+  let adminUser = await users.findOne({ username: "admin" });
+  if (!adminUser) {
+    const now = new Date();
+    const hashPassword = utils.createHashPassword({ password: "Welcome110" });
+    const guid = utils.generateGUID();
+
+    adminUser = {
+      createdAt: now,
+      updatedAt: now,
+      lastSeen: now,
+      firstName: "main",
+      lastName: "admin",
+      mobile: "",
+      email: "",
+      username: "admin",
+      password: hashPassword,
+      guid,
+      roles: [userModel.statics.roles.Admin],
+      status: {
+        isTrash: false,
+        isSuspend: false,
+        isActive: false,
+      },
+      token: {
+        code: "EXPIRED_TOKEN",
+        expiredAt: now,
+      },
+    };
+
+    await services.users.addNewUser({ user: adminUser });
+  }
+};
